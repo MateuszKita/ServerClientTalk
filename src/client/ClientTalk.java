4e32625a08b8382fc93d884e6226dbb761615efc
MateuszKita <mkita.zg@gmail.com>
2018-06-02 14:52:18 +0200

    GUI and changing value of progress bar by 1 client implemented

--- src/client/ClientTalk.java
+++ src/client/ClientTalk.java
@@ -1,14 +1,32 @@
 package client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import static java.lang.Thread.sleep;
 import java.net.Socket;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ClientTalk extends javax.swing.JFrame {
 
-    static String plusMinusSign = "+";
+    public String plusMinusSign = randomPlusMinus();
 
     public ClientTalk() {
         initComponents();
+    }
+
+    String randomPlusMinus() {
+
+        Random rand = new Random();
+        int n = rand.nextInt(2) + 1;
+        if (n == 1) {
+            return "+";
+        } else {
+            return "-";
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -19,6 +37,8 @@ public class ClientTalk extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jProgressBar1 = new javax.swing.JProgressBar();
         connectButton = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        console = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -29,6 +49,15 @@ public class ClientTalk extends javax.swing.JFrame {
         jButton1.setFont(new java.awt.Font("Dialog", 1, 60)); // NOI18N
         jButton1.setText(plusMinusSign
         );
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                try {
+                    jButton1ActionPerformed(evt);
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        });
 
         connectButton.setText("Connect");
         connectButton.addActionListener(new java.awt.event.ActionListener() {
@@ -36,6 +65,10 @@ public class ClientTalk extends javax.swing.JFrame {
                 connectButtonActionPerformed(evt);
             }
         });
+
+        console.setColumns(20);
+        console.setRows(5);
+        jScrollPane1.setViewportView(console);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -45,42 +78,44 @@ public class ClientTalk extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(24, 24, 24)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jProgressBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 344, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 362, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(157, 157, 157)
-                        .addComponent(connectButton)))
-                .addContainerGap(25, Short.MAX_VALUE))
+                        .addGap(143, 143, 143)
+                        .addComponent(connectButton))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(123, 123, 123)
+                        .addComponent(jLabel1)))
+                .addContainerGap(7, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 268, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(62, 62, 62))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 348, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 257, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(33, 33, 33)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(connectButton)
-                .addContainerGap(31, Short.MAX_VALUE))
+                .addGap(25, 25, 25))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void connectButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_connectButtonActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_connectButtonActionPerformed
-
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(() -> {
-            new ClientTalk().setVisible(true);
-        });
-
         try {
+            jButton1.setEnabled(false);
             Socket sock = new Socket("localhost", 8000);
             SendToServer sendToServer = new SendToServer(sock);
             Thread sendThread = new Thread(sendToServer);
@@ -91,12 +126,98 @@ public class ClientTalk extends javax.swing.JFrame {
         } catch (IOException exception) {
             System.out.println(exception.getMessage());
         }
+    }//GEN-LAST:event_connectButtonActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) throws IOException {
+        if (plusMinusSign == "-") {
+            jProgressBar1.setValue(jProgressBar1.getValue() - 1);
+        }
+        if (plusMinusSign == "+") {
+            jProgressBar1.setValue(jProgressBar1.getValue() + 1);
+        }
+    }
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new ClientTalk().setVisible(true);
+        });
+    }
+
+    public class SendToServer implements Runnable {
+
+        Socket sock = null;
+        PrintWriter print = null;
+        BufferedReader brinput = null;
+
+        public SendToServer(Socket sock) {
+            this.sock = sock;
+        }
+
+        @Override
+        public void run() {
+            try {
+                if (sock.isConnected()) {
+                    try {
+                        sleep(100);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(ClientTalk.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    console.append("Client connected to " + sock.getInetAddress() + " on port " + sock.getPort());
+                    this.print = new PrintWriter(sock.getOutputStream(), true);
+                    while (true) {
+                        brinput = new BufferedReader(new InputStreamReader(System.in));
+                        this.print.println(Integer.toString(jProgressBar1.getValue()));
+                        this.print.flush();
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public class ReceiveFromServer implements Runnable {
+
+        Socket sock = null;
+        BufferedReader recieve = null;
+
+        public ReceiveFromServer(Socket sock) {
+            this.sock = sock;
+        }
+
+        @Override
+        public void run() {
+            try {
+                recieve = new BufferedReader(new InputStreamReader(this.sock.getInputStream()));
+                String msgRecieved = null;
+                console.append(recieve.readLine());
+                while ((msgRecieved = recieve.readLine()) != null) {
+                    if (jProgressBar1.getValue() != Integer.parseInt(msgRecieved)) {
+                        console.append("\nProgress bar value from server: " + msgRecieved);
+                        jProgressBar1.setValue(Integer.parseInt(msgRecieved));
+                    }
+                    if (jProgressBar1.getValue() != 100 || jProgressBar1.getValue() != 0) {
+                        jButton1.setEnabled(true);
+                    }
+                    if (Integer.parseInt(msgRecieved) == 0 || Integer.parseInt(msgRecieved) == 100) {
+                        console.append("\nTHE END!!");
+                        jButton1.setEnabled(false);
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton connectButton;
+    public static javax.swing.JTextArea console;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JProgressBar jProgressBar1;
+    static javax.swing.JProgressBar jProgressBar1;
+    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 }
--- src/server/ServerTalk.java
+++ src/server/ServerTalk.java
@@ -1,14 +1,23 @@
 package server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import static java.lang.Thread.sleep;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ServerTalk extends javax.swing.JFrame {
 
     public ServerTalk() {
         initComponents();
     }
+
+    public int port = 8000;
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -18,7 +27,7 @@ public class ServerTalk extends javax.swing.JFrame {
         progressBar = new javax.swing.JProgressBar();
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        console = new javax.swing.JTextArea();
         startServerButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -36,14 +45,20 @@ public class ServerTalk extends javax.swing.JFrame {
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("Server");
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
+        console.setColumns(20);
+        console.setRows(5);
+        jScrollPane1.setViewportView(console);
 
-        startServerButton.setText("Start Server");
+        startServerButton.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        startServerButton.setText("Run Server");
+
         startServerButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                startServerButtonActionPerformed(evt);
+                try {
+                    startServerButtonActionPerformed(evt);
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
             }
         });
 
@@ -60,8 +75,8 @@ public class ServerTalk extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 312, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(startServerButton)
-                        .addGap(113, 113, 113)))
+                        .addComponent(startServerButton, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(60, 60, 60)))
                 .addContainerGap(33, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -71,15 +86,11 @@ public class ServerTalk extends javax.swing.JFrame {
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(progressBar, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(39, 39, 39)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(39, 39, 39)
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(startServerButton)
-                        .addGap(40, 40, 40)))
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(startServerButton, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 243, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(27, Short.MAX_VALUE))
         );
@@ -87,19 +98,10 @@ public class ServerTalk extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void startServerButtonActionPerformed(java.awt.event.ActionEvent evt) {
-        // TODO add your handling code here:
-    }
-
-    public static void main(String args[]) throws IOException {
-        java.awt.EventQueue.invokeLater(() -> {
-            new ServerTalk().setVisible(true);
-        });
-
-        final int port = 8000;
+    private void startServerButtonActionPerformed(java.awt.event.ActionEvent evt) throws IOException {
         ServerSocket serverSocket = new ServerSocket(port);
         Socket clientSocket = serverSocket.accept();
-        System.out.println("Received connection from " + clientSocket.getInetAddress() + " on port " + clientSocket.getPort());
+        console.append("Received connection from " + clientSocket.getInetAddress() + " on port " + clientSocket.getPort());
 
         ReceiveFromClient receive = new ReceiveFromClient(clientSocket);
         Thread receiveThread = new Thread(receive);
@@ -110,12 +112,87 @@ public class ServerTalk extends javax.swing.JFrame {
         sendThread.start();
     }
 
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new ServerTalk().setVisible(true);
+        });
+    }
+
+    public class ReceiveFromClient implements Runnable {
+
+        Socket clientSocket = null;
+        BufferedReader brBufferedReader = null;
+
+        public ReceiveFromClient(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                sleep(100);
+                try {
+                    brBufferedReader = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+                    while (true) {
+                        String msgRecieved = brBufferedReader.readLine();
+                        if (progressBar.getValue() != Integer.parseInt(msgRecieved)) {
+                            console.append("\nProgress bar value from client: " + msgRecieved);
+                            progressBar.setValue(Integer.parseInt(msgRecieved));
+                        }
+                    }
+
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ServerTalk.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public class SendToClient implements Runnable {
+
+        PrintWriter pwPrintWriter;
+        Socket clientSock = null;
+
+        public SendToClient(Socket clientSock) {
+            this.clientSock = clientSock;
+        }
+
+        @Override
+        public void run() {
+            try {
+                pwPrintWriter = new PrintWriter(new OutputStreamWriter(this.clientSock.getOutputStream()));
+                if (clientSock.isConnected()) {
+                    console.append("\nNew client connected");
+                    pwPrintWriter.flush();
+                }
+                while (true) {
+
+                    if (progressBar.getValue() == 100 || progressBar.getValue() == 100) {
+                        console.append("\nTHE END!!");
+                        break;
+                    }
+                    pwPrintWriter.println(Integer.toString(progressBar.getValue()));
+                    pwPrintWriter.flush();
+                    try {
+                        sleep(120);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(ServerTalk.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    static javax.swing.JTextArea console;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JProgressBar progressBar;
+    static javax.swing.JProgressBar progressBar;
     private javax.swing.JButton startServerButton;
     // End of variables declaration//GEN-END:variables
 }
